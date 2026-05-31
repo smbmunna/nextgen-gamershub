e5c7998ab40ae5a63bb5f59bec70e5a2b6988ff7
smbmunna <smbmunna@gmail.com>
2026-05-31 15:45:48 +0600

    Platform component type fix

--- src/components/Platform.tsx
+++ src/components/Platform.tsx
@@ -4,19 +4,22 @@ import { IoIosArrowDropdown } from "react-icons/io";
 import { getAllData } from "../services/getAllData";
 import PlatformList from "./PlatformList";
 
+export interface Platform{
+    id: number; 
+    name: string; 
+    slug: string
+}
+
 
 export default async function Platform() {
 
     const platforms = await getAllData('platforms/lists/parents', '');
-
-
-
     return (
         <div className="dropdown dropdown-bottom">
             <div tabIndex={0} role="button" className="btn m-1">Platform <IoIosArrowDropdown fontSize={20} /></div>
-            <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52  shadow-sm">
+            <ul tabIndex={-1} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52  shadow-sm">
                 {
-                    platforms.results.map(pl => <PlatformList platform={pl} key={pl.id} />)
+                    platforms.results.map((pl: Platform) => <PlatformList platform={pl} key={pl.id} />)
 
                 }
             </ul>
