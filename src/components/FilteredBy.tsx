b58bfe1c1f310990e2efd1eec9582bac14de94dd
smbmunna <smbmunna@gmail.com>
2026-05-31 18:12:21 +0600

    Gamegrid and FilteredBy lable updated.

--- src/components/FilteredBy.tsx
+++ src/components/FilteredBy.tsx
@@ -2,27 +2,44 @@
 import { useSearchParams } from "next/navigation";
 import { getGenreName } from "../services/getGenreName";
 
-export default function FilteredBy() {
-  const searchParams = useSearchParams();
-  const paramsArray = Array.from(searchParams.entries());
-  const hasParams = searchParams.toString() !== "";
+interface FilteredByProps {
+  genreName: string;
+  platformName: string;
+}
 
-  if (!hasParams) return null;
+export default function FilteredBy({
+  genreName,
+  platformName,
+}: FilteredByProps) {
+  if (!genreName && !platformName) return null;
 
-  const formatKey= (key: string)=>{
-    if(key==='parent_platform') return 'Platform'; 
-    if(key==='genres') return 'Genre'
-  }
+  // const searchParams = useSearchParams();
+  // const paramsArray = Array.from(searchParams.entries());
+  // const hasParams = searchParams.toString() !== "";
 
-  
+  // if (!hasParams) return null;
+
+  // const formatKey= (key: string)=>{
+  //   if(key==='parent_platform') return 'Platform';
+  //   if(key==='genres') return 'Genre'
+  // }
+
   return (
     <div className=" text-black p-2 flex gap-2 items-center">
       <span className="text-white bg-gray-700 px-2">Filtered By:</span>
-      {
+      {/* {
         paramsArray.map(([key, value])=>(
             <div className="bg-green-200 text-black p-1" key={key}>{formatKey(key)}: {value}</div>
         ))
-      }
+      } */}
+      {genreName && (
+        <div className="bg-green-200 text-black p-1">Genre: {genreName}</div>
+      )}
+      {platformName && (
+        <div className="bg-green-200 text-black p-1">
+          Platform: {platformName}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -20,12 +20,26 @@ export default async function GameGrid({ genreId, platformId }: GameGridProps) {
     //const params= genreId? `&genres=${genreId}` : ''; 
     const games = await getAllData('games', params.toString());
 
+    let activeGenreName=''; 
+    let activePlatformName=''; 
+
+    if(genreId){
+        const data= await getAllData(`genres/${genreId}`, ''); 
+        activeGenreName= data?.name || ''; 
+    }
+
+    if(platformId){
+        const data= await getAllData('platforms/lists/parents', ''); 
+        const matchedPlatform= data.results?.find((pl: any)=> pl.id==platformId); 
+        activePlatformName= matchedPlatform?.name; 
+    }
+
 
     return (
         <div className="ml-4">
             <div className="flex gap-4 items-center">
                 <Platform />
-                <FilteredBy/>
+                <FilteredBy genreName= {activeGenreName} platformName={activePlatformName}/>
                 <ClearFilter />
             </div>
             <h2 className="mb-8 font-semibold text-2xl">Games</h2>
