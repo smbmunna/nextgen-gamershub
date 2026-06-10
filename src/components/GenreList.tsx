ac3d0cc7e15e83a544e5f649f5d669739748e441
smbmunna <smbmunna@gmail.com>
2026-06-10 14:27:01 +0600

    BG change on selected genres.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,25 +4,31 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { Genre } from "./GameCard";
 
 interface Props {
-  genre: Genre
+  genre: Genre;
 }
 
 export default function GenreList({ genre }: Props) {
   const router = useRouter();
   const searchParams = useSearchParams();
+
+  const isSelected = searchParams.get("genres") === String(genre.id);
+  console.log(isSelected);
+
   const handleClick = (id: number) => {
     const params = new URLSearchParams(searchParams.toString());
+
     params.set("genres", String(id));
     router.push(`/?${params.toString()}`);
   };
   return (
     <li
+      
       onClick={() => {
         handleClick(genre.id);
       }}
       key={genre.id}
     >
-      <a>{genre.name}</a>
+      <a className={isSelected ? 'badge-soft badge-accent font-semibold text-md' : ''}>{genre.name}</a>
     </li>
   );
 }
