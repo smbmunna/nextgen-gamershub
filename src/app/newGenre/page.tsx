6daaed33118deaf7b98a1d5c416ebd15809943ae
smbmunna <smbmunna@gmail.com>
2026-07-01 16:39:46 +0600

    Add genre entry form

--- src/app/newGenre/page.tsx
+++ src/app/newGenre/page.tsx
@@ -1,9 +1,21 @@
+"use client";
 import Navbar from "@/src/components/Navbar";
 
 export default function NewGame() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
   return (
-    <main className="min-h-full flex flex-col p-4">
-      <h2>Create New game</h2>
+    <main className="">
+      <div className="w-1/2 mx-auto mt-20">
+        <h2 className="text-center text-2xl font-medium ">Create New game</h2>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
+          <input type="text" placeholder="Genre Name" className="input mx-auto  border-gray-400" />          
+          <button className="btn btn-success w-20 mx-auto ">Submit</button>
+        </form>
+      </div>
     </main>
   );
 }
